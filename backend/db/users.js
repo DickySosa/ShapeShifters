75dfe09d90d00f2a71760b460662e7f90fe3803c
DickySosa <dicky5217@gmail.com>
2023-07-04 15:33:54 -0600

    Adding CRUD operations

--- backend/db/users.js
+++ backend/db/users.js
@@ -22,23 +22,50 @@ const verifyUser = (client, username, password) => {
     return client.query(signInQuery)
 }
 
-const getAllUsers = () => {
-
+const getAllUsers = (client) => {
+    const getAllUsersQuery = {
+        text : `SELECT * FROM ${TABLE_NAME}`
+    }
+    console.log(getAllUsersQuery)
+    return client.query(getAllUsersQuery)
 }
 
-const getUserById = () => {
-
+const getUserById = (client, id) => {
+    const getUsersByIdQuery = {
+        text : `SELECT id, username, email, password FROM ${TABLE_NAME} WHERE id = ${id}`
+    }
+    console.log(getUsersByIdQuery)
+    return client.query(getUsersByIdQuery)
 }
 
-const updateUser = () => {
-
+const updateUser = (client, id, username, email, password) => {
+    const updateUserQuery = {
+        text : 
+        `UPDATE ${TABLE_NAME} 
+        SET
+        id =  ${id},
+        username = ${username},
+        email = ${email},
+        password =  ${password}
+        WHERE id = ${id}`
+    }
+    console.log(updateUserQuery)
+    return client.query(updateUserQuery)
 }
 
-const deleteUser = () => {
-
+const deleteUser = (client, id) => {
+    const deleteUserQuery = {
+        text : `DELETE FROM ${TABLE_NAME} WHERE id = ${id}`
+    }
+    console.log(deleteUserQuery)
+    return client.query(deleteUserQuery)
 }
 
 module.exports = {
     createUser,
     verifyUser,
+    getAllUsers,
+    getUserById,
+    updateUser,
+    deleteUser,
 }
